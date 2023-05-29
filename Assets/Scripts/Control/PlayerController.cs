973ae19925228c5ec3a1b9e190f5f5d1bab1dc7b
Joshua Torrington-Smith <59626141+CatSandwich@users.noreply.github.com>
2023-05-29 14:12:07 -0400

    Running

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -5,21 +5,24 @@ namespace Control
     public class PlayerController : MonoBehaviour
     {
         [SerializeField] [Range(0, 5)] private float m_MoveSpeed = 3.5f;
+        [SerializeField] [Range(0, 10)] private float m_RunMoveSpeed = 3.5f;
         [SerializeField] [Range(0, 5)] private float m_TurnSpeed = 1.5f;
         [SerializeField] private Transform m_PlayerChestTransform;
 
         private Animator m_Animator;
 
         private bool m_IsGrounded = true;
+
+        private bool IsRunning => Input.GetKey(KeyCode.LeftShift);
         
         #region Animator Hashes
-        private static readonly int IsIdle = Animator.StringToHash("IsIdle");
-        private static readonly int IsWalking = Animator.StringToHash("IsWalking");
-        private static readonly int IsBackwards = Animator.StringToHash("IsBackwards");
-        private static readonly int IsRightStrafing = Animator.StringToHash("IsRightStrafing");
-        private static readonly int IsLeftStrafing = Animator.StringToHash("IsLeftStrafing");
-        private static readonly int IsArmed = Animator.StringToHash("IsArmed");
-        private static readonly int IsRunning = Animator.StringToHash("IsRunning");
+        private static readonly int Anim_IsIdle = Animator.StringToHash("IsIdle");
+        private static readonly int Anim_IsWalking = Animator.StringToHash("IsWalking");
+        private static readonly int Anim_IsBackwards = Animator.StringToHash("IsBackwards");
+        private static readonly int Anim_IsRightStrafing = Animator.StringToHash("IsRightStrafing");
+        private static readonly int Anim_IsLeftStrafing = Animator.StringToHash("IsLeftStrafing");
+        private static readonly int Anim_IsArmed = Animator.StringToHash("IsArmed");
+        private static readonly int Anim_IsRunning = Animator.StringToHash("IsRunning");
         #endregion
 
         private void Awake()
@@ -29,6 +32,7 @@ namespace Control
 
         private void Start()
         {
+            m_Animator.SetBool(Anim_IsArmed, true);
             Cursor.lockState = CursorLockMode.Locked;
         }
 
@@ -57,30 +61,19 @@ namespace Control
             Vector3 absoluteDirection = new Vector3(horizontal, 0f, vertical);
             absoluteDirection.Normalize();
 
-            ResetAllBools();
             UpdateAnimatorBools(absoluteDirection);
 
-            transform.Translate(absoluteDirection * (m_MoveSpeed * Time.deltaTime));
+            transform.Translate(absoluteDirection * (Time.deltaTime * (IsRunning ? m_RunMoveSpeed : m_MoveSpeed)));
         }
 
         private void UpdateAnimatorBools(Vector3 direction)
         {
-            if (direction.magnitude == 0f) m_Animator.SetBool(IsIdle, true);
-            if (direction.magnitude > 0f) m_Animator.SetBool(IsWalking, true);
-            if (direction.z < 0f) m_Animator.SetBool(IsBackwards, true);
-            if (direction.x > 0f) m_Animator.SetBool(IsRightStrafing, true);
-            if (direction.x < 0f) m_Animator.SetBool(IsLeftStrafing, true);
-        }
-
-        private void ResetAllBools()
-        {
-            m_Animator.SetBool(IsIdle, false);
-            m_Animator.SetBool(IsArmed, true);
-            m_Animator.SetBool(IsWalking, false);
-            m_Animator.SetBool(IsRunning, false);
-            m_Animator.SetBool(IsBackwards, false);
-            m_Animator.SetBool(IsRightStrafing, false);
-            m_Animator.SetBool(IsLeftStrafing, false);
+            m_Animator.SetBool(Anim_IsIdle, direction.magnitude == 0f);
+            m_Animator.SetBool(Anim_IsWalking, direction.magnitude > 0f);
+            m_Animator.SetBool(Anim_IsRunning, IsRunning);
+            m_Animator.SetBool(Anim_IsBackwards, direction.z < 0f);
+            m_Animator.SetBool(Anim_IsRightStrafing, direction.x > 0f);
+            m_Animator.SetBool(Anim_IsLeftStrafing, direction.x < 0f);
         }
 
         public Transform GetTargetTransform()
