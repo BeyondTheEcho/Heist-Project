0118b08ba34fa21ebc91562747b3f3f3cafed7d9
Stelios Klapsis <Steliosklapsis@outlook.com>
2023-05-29 10:42:17 -0400

    Setup Initial Player Animator

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -2,17 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace Control
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerController : MonoBehaviour
     {
-        
-    }
+        [SerializeField] [Range(0, 5)] private float m_MoveSpeed = 3.5f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private CharacterController m_Controller;
+        private Animator m_Animator;
+
+        private void Awake()
+        {
+            m_Controller = GetComponent<CharacterController>();
+            m_Animator = GetComponent<Animator>();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+            Debug.Log(direction);
+
+            ResetAllBools();
+            UpdateAnimatorBools(direction);
+
+            m_Controller.Move(direction * m_MoveSpeed * Time.deltaTime);
+        }
+
+        private void UpdateAnimatorBools(Vector3 direction)
+        {
+            if (direction.magnitude == 0f) m_Animator.SetBool("IsIdle", true);
+            if (direction.magnitude > 0f) m_Animator.SetBool("IsWalking", true);
+            if (direction.z < 0f) m_Animator.SetBool("IsBackwards", true);
+        }
+
+        private void ResetAllBools()
+        {
+            m_Animator.SetBool("IsIdle", false);
+            m_Animator.SetBool("IsArmed", false);
+            m_Animator.SetBool("IsWalking", false);
+            m_Animator.SetBool("IsRunning", false);
+            m_Animator.SetBool("IsBackwards", false);
+        }
     }
 }
