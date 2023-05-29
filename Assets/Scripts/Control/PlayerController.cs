da4f1e0abcedaeae1e4d006c07787c1cdc2e435a
Joshua Torrington-Smith <59626141+CatSandwich@users.noreply.github.com>
2023-05-29 12:28:42 -0400

    Player movement & font change

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -7,9 +7,20 @@ namespace Control
     public class PlayerController : MonoBehaviour
     {
         [SerializeField] [Range(0, 5)] private float m_MoveSpeed = 3.5f;
+        [SerializeField] [Range(0, 5)] private float m_TurnSpeed = 1.5f;
 
         private CharacterController m_Controller;
         private Animator m_Animator;
+        
+        #region Animator Hashes
+        private static readonly int IsIdle = Animator.StringToHash("IsIdle");
+        private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+        private static readonly int IsBackwards = Animator.StringToHash("IsBackwards");
+        private static readonly int IsRightStrafing = Animator.StringToHash("IsRightStrafing");
+        private static readonly int IsLeftStrafing = Animator.StringToHash("IsLeftStrafing");
+        private static readonly int IsArmed = Animator.StringToHash("IsArmed");
+        private static readonly int IsRunning = Animator.StringToHash("IsRunning");
+        #endregion
 
         private void Awake()
         {
@@ -17,46 +28,43 @@ namespace Control
             m_Animator = GetComponent<Animator>();
         }
 
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
         void Update()
         {
-            float horizontal = Input.GetAxis("Horizontal");
-            float vertical = Input.GetAxis("Vertical");
+            var v = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            transform.Rotate(Vector3.up, v.x * m_TurnSpeed);
+            
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
 
-            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-            Debug.Log(direction);
+            Vector3 absoluteDirection = new Vector3(horizontal, 0f, vertical);
+            absoluteDirection.Normalize();
+            Vector3 relativeDirection = horizontal * transform.right + vertical * transform.forward;
+            relativeDirection.Normalize();
 
             ResetAllBools();
-            UpdateAnimatorBools(direction);
+            UpdateAnimatorBools(absoluteDirection);
 
-            m_Controller.Move(direction * m_MoveSpeed * Time.deltaTime);
+            m_Controller.Move(relativeDirection * (m_MoveSpeed * Time.deltaTime));
         }
 
         private void UpdateAnimatorBools(Vector3 direction)
         {
-            if (direction.magnitude == 0f) m_Animator.SetBool("IsIdle", true);
-            if (direction.magnitude > 0f) m_Animator.SetBool("IsWalking", true);
-            if (direction.z < 0f) m_Animator.SetBool("IsBackwards", true);
-            if (direction.x > 0f) m_Animator.SetBool("IsRightStrafing", true);
-            if (direction.x < 0f) m_Animator.SetBool("IsLeftStrafing", true);
+            if (direction.magnitude == 0f) m_Animator.SetBool(IsIdle, true);
+            if (direction.magnitude > 0f) m_Animator.SetBool(IsWalking, true);
+            if (direction.z < 0f) m_Animator.SetBool(IsBackwards, true);
+            if (direction.x > 0f) m_Animator.SetBool(IsRightStrafing, true);
+            if (direction.x < 0f) m_Animator.SetBool(IsLeftStrafing, true);
         }
 
         private void ResetAllBools()
         {
-            m_Animator.SetBool("IsIdle", false);
-            m_Animator.SetBool("IsArmed", true);
-            m_Animator.SetBool("IsWalking", false);
-            m_Animator.SetBool("IsRunning", false);
-            m_Animator.SetBool("IsBackwards", false);
-            m_Animator.SetBool("IsRightStrafing", false);
-            m_Animator.SetBool("IsLeftStrafing", false);
+            m_Animator.SetBool(IsIdle, false);
+            m_Animator.SetBool(IsArmed, true);
+            m_Animator.SetBool(IsWalking, false);
+            m_Animator.SetBool(IsRunning, false);
+            m_Animator.SetBool(IsBackwards, false);
+            m_Animator.SetBool(IsRightStrafing, false);
+            m_Animator.SetBool(IsLeftStrafing, false);
         }
     }
 }
